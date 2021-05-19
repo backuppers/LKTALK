fd3d863c3b5d4f59118050bf8610885e29fa08dd
mj2 <mj2@mj2sAir.local>
2021-05-19 23:09:26 +0900

    add auto pulling shell

---
+++ pullloop.sh
@@ -0,0 +1,12 @@
+
+while true;
+do
+  local=$(git rev-parse HEAD)
+  target=$(git rev-parse origin)
+  if [ $local != $target ]; then
+    git pull
+    echo pulling now $(date)
+  fi
+  echo waiting 10 sec...
+  sleep 10;
+done
